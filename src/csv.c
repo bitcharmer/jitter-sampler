b0661dd7713c988adde2210823dbda1721537f04
Wojciech Kudla <wojciech.kudla@gmail.com>
2017-04-02 08:20:12 +0100

    csv output fixes

--- src/csv.c
+++ src/csv.c
@@ -4,6 +4,13 @@
 
 FILE *f;
 
+void write_to_file(unsigned long long size, struct jitter *results) {
+    for (long long i = 0; i < size; i++)
+        fprintf(f, "%llu,%llu\n", results[i].timestamp, results[i].delay);
+
+    fclose(f);
+}
+
 process_output init_csv(char* path) {
     f = fopen(path, "w");
     if (f == NULL) {
@@ -13,11 +20,5 @@ process_output init_csv(char* path) {
 
     printf("Writing results to csv file: %s\n", path);
     fprintf(f, "time,delay\n");
-}
-
-void write_to_file(unsigned long long size, struct jitter *results) {
-    for (long long i = 0; i < size; i++)
-        fprintf(f, "%llu,%llu\n", results[i].timestamp, results[i].delay);
-
-    fflush(f);
+    return &write_to_file;
 }

f4069e66b7d8933e4ebb6ef4561e914a3dd7b712
Wojciech Kudla <wojciech.kudla@gmail.com>
2020-06-30 11:10:07 +0100

    major clean-up, added parametrised timestamp capture methods

--- src/jitter.h
+++ src/jitter.h
@@ -3,11 +3,22 @@
 
 #include <stdint.h>
 
+typedef void (*process_output) (int64_t, struct jitter*, int64_t);
+typedef int64_t (*time_func) (void);
+
 struct jitter {
     int64_t timestamp;
     int64_t delay;
 };
 
-typedef void (*process_output) (int64_t, struct jitter*, int64_t);
+struct program_args {
+    _Bool hist_mode;
+    _Bool disable_irqs;
+    int64_t duration;
+    int64_t granularity;
+    int cpu;
+    process_output process_output;
+    time_func time_func;
+};
 
 #endif //JITTER_H
--- src/main.c
+++ src/main.c
@@ -7,24 +7,29 @@
 #include <stdlib.h>
 #include <sched.h>
 #include <error.h>
+#include <errno.h>
 #include <numa.h>
 #include <sys/mman.h>
 #include <sys/io.h>
 
-
 #define NANOS_IN_SEC 1000000000ul
 
-struct timespec ts;
+long double cpu_freq = 0;
+unsigned long long time_offset;
 
-void print_args(int64_t duration, int64_t granularity, int64_t cpu);
-void affinitze_to_cpu(int64_t cpu) ;
-void parse_args(int argc, char *const *argv, int64_t *duration, int64_t *granularity, int64_t *cpu, process_output *process_output);
+void print_args(struct program_args *args);
+void affinitze_to_cpu(unsigned int cpu) ;
+void parse_args(int argc, char *const *argv, struct program_args *args);
 
 
 void print_usage() {
     puts("Usage:\n");
     puts("-h\tdisplay this usage message");
     puts("-c\ttarget cpu to run on (default: any)");
+    puts("-t\ttimestamp capturing method (default: realtime). Allowed values:"
+         "\n\trealtime (uses clock_gettime(CLOCK_REALTIME),"
+         "\n\trdtsc (uses RDTSC instruction, requires passing target CPU frequency with -f argument");
+    puts("-f\target cpu frequency in GHz as decimal number");
     puts("-d\tsampling duration in seconds (default: 60)");
     puts("-r\tjitter reporting interval in milliseconds (default: 1000)");
     puts("-o\toutput results using an output plugin. Supported plugins:");
@@ -35,25 +40,45 @@ void print_usage() {
 }
 
 
+static __inline__ int64_t rdtsc(void) {
+    unsigned hi, lo;
+    __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+    return (( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 ));
+}
+
+static __inline__ int64_t rdtscp_time(void)
+{
+    unsigned long long tsc;
+
+    __asm__ __volatile__(
+    "rdtscp;"
+    "shl $32, %%rdx;"
+    "or %%rdx, %%rax"
+    : "=a"(tsc)
+    :
+    : "%rcx", "%rdx");
+
+    return tsc / cpu_freq;
+}
+
+static __inline__ int64_t rdtsc_time(void) {
+        return rdtsc() / cpu_freq + time_offset;
+}
+
 static inline int64_t nano_time() {
+    static struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
     return ts.tv_sec * NANOS_IN_SEC + ts.tv_nsec;
 }
 
-static __inline__ unsigned long long rdtsc_time(void) {
-    unsigned hi, lo;
-    __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
-    return (( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 )) / 2.5;
-}
-
-int64_t capture_jitter(int64_t duration, int64_t granularity, struct jitter *jitter) {
-    int64_t ts = nano_time();
+int64_t capture_jitter(int64_t duration, int64_t granularity, struct jitter *jitter, time_func time_func) {
+    int64_t ts = time_func();
     int64_t deadline = ts + duration;
     int64_t next_report = ts + granularity;
     int64_t idx = 0, max = 0;
 
     while (ts < deadline) {
-        int64_t now = rdtsc();
+        int64_t now = time_func();
         int64_t latency = now - ts;
 
         if (latency > max) max = latency;
@@ -61,8 +86,8 @@ int64_t capture_jitter(int64_t duration, int64_t granularity, struct jitter *jit
         if (now > next_report) {
             jitter[idx++] = (struct jitter) {.timestamp = now, .delay = max};
             max = 0;
+            now = time_func();
             next_report = now + granularity;
-            now = nano_time();
         }
 
         ts = now;
@@ -73,82 +98,110 @@ int64_t capture_jitter(int64_t duration, int64_t granularity, struct jitter *jit
 
 
 int main(int argc, char* argv[]) {
-    int64_t duration = 60 * NANOS_IN_SEC;
-    int64_t granularity = NANOS_IN_SEC;
-    int64_t cpu = 0;
-    process_output process_output;
+    struct program_args args = {
+            .hist_mode = 0,
+            .disable_irqs = 0,
+            .duration = 60 * NANOS_IN_SEC,
+            .granularity = NANOS_IN_SEC,
+            .cpu = -1,
+            .process_output = NULL,
+            .time_func = nano_time
+    };
 
-    parse_args(argc, argv, &duration, &granularity, &cpu, &process_output);
-    print_args(duration, granularity, cpu);
-    affinitze_to_cpu(cpu);
+    parse_args(argc, argv, &args);
+    print_args(&args);
 
-    puts("MLOCKING AAARRR everything");
+    if (args.cpu >= 0) affinitze_to_cpu(args.cpu);
+
     int ret = mlockall(MCL_CURRENT | MCL_FUTURE);
     if (ret) {
-        puts("OMG, MLOCK didn't work!");
+        puts("Warning: mlockall() failed. This may impact the results");
     }
 
-    int64_t items = duration / granularity;
+    int64_t items = args.duration / args.granularity;
     size_t sz = items * sizeof(struct jitter);
-    printf("mmapping %lu KB\n", sz / 1024);
-    struct jitter* jitter = mmap(NULL, sz, PROT_WRITE | PROT_READ, MAP_HUGETLB | MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE | MAP_LOCKED, 0, 0);
+    printf("Mmapping %lu KB for data\n", sz / 1024);
+    struct jitter *jitter = mmap(NULL, sz, PROT_WRITE | PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE | MAP_LOCKED, 0, 0);
 
-    if (jitter == NULL) {
-        puts("Ooops");
-        exit(1);
+    if (jitter == MAP_FAILED) {
+        error(1, errno, "Mmapping failed");
     }
 
-    puts("MADVISE");
     madvise(jitter, sz, MADV_SEQUENTIAL);
 
-    puts("Prefaulting");
-    for (int i = 0; i < items; i++)
-        jitter[i].timestamp = 0;
+    if (args.disable_irqs && args.cpu >= 0) {
+        printf("Disabling IRQs on cpu %i (this may require root)", args.cpu);
+        int err = iopl(3);
+        if (err)
+            error(1, errno, "Error while changing privilege level with iopl()");
 
-    puts("Disabling irqs and running shit");
-    iopl(3);
-    asm("cli");
-    capture_jitter(1000000000L, 1000000L, jitter);
-    int64_t data_len = capture_jitter(duration, granularity, jitter);
+        asm("cli");
+    }
 
-    asm("sti");
+    if (args.time_func == rdtsc_time) {
+        if (cpu_freq == 0)
+            error(1, 0, "Using RDTSC-based time requires passing cpu frequency with -f argument");
 
-    printf("Finished with %li results\n", data_len);
-    process_output(data_len, jitter, cpu);
+        int64_t now = nano_time();
+        time_offset = now - rdtscp_time();
+    }
+
+    // code/data warm-up
+    capture_jitter(1000000000L, 1000000L, jitter, args.time_func);
+
+    puts("Running jitter measurement");
+    int64_t data_len = capture_jitter(args.duration, args.granularity, jitter, args.time_func);
+
+    if (args.disable_irqs && args.cpu >= 0) {
+        // re-enable IRQs
+        asm("sti");
+    }
+
+    printf("Finished measurement. Captured %li data points\n", data_len);
+    args.process_output(data_len, jitter, args.cpu);
     munmap(jitter, sz);
-
-    return 0;
 }
 
-
-void parse_args(int argc, char *const *argv, int64_t *duration, int64_t *granularity, int64_t *cpu, process_output *process_output) {
+void parse_args(int argc, char *const *argv, struct program_args *args) {
     for (int idx = 1; idx < argc; idx++) {
         if (strcmp("-h", argv[idx]) == 0) print_usage();
-        else if (strcmp("-c", argv[idx]) == 0) (*cpu) = strtol(argv[++idx], (char **)NULL, 10);
-        else if (strcmp("-d", argv[idx]) == 0) (*duration) = strtol(argv[++idx], (char **)NULL, 10) * NANOS_IN_SEC;
-        else if (strcmp("-r", argv[idx]) == 0) (*granularity) = strtol(argv[++idx], (char **)NULL, 10) * 1000000ul;
+        else if (strcmp("-histo", argv[idx]) == 0) args->hist_mode = 1;
+        else if (strcmp("-c", argv[idx]) == 0) args->cpu = strtol(argv[++idx], (char **)NULL, 10);
+        else if (strcmp("-d", argv[idx]) == 0) args->duration = strtol(argv[++idx], (char **)NULL, 10) * NANOS_IN_SEC;
+        else if (strcmp("-r", argv[idx]) == 0) args->granularity = strtol(argv[++idx], (char **)NULL, 10) * 1000000ul;
         else if (strcmp("-o", argv[idx]) == 0) {
             char *output = argv[++idx];
-            if (strstr(output, "influx://")) (*process_output) = init_influx(output + 9);
-            else if (strstr(output, "csv://")) (*process_output) = init_csv(output + 6);
+            if (strstr(output, "influx://")) args->process_output = init_influx(output + 9);
+            else if (strstr(output, "csv://")) args->process_output = init_csv(output + 6);
         }
+        else if (strcmp("-t", argv[idx]) == 0) {
+            char *time_method = argv[++idx];
+            if (strstr(time_method, "rdtsc")) args->time_func = rdtsc_time;
+        }
+        else if (strcmp("-f", argv[idx]) == 0) {
+            char *freq_str = argv[++idx];
+            cpu_freq = strtold(freq_str, NULL);
+        }
+        else if (strcmp("-noirqs", argv[idx]) == 0) args->disable_irqs = 1;
     }
 }
 
 
-void affinitze_to_cpu(int64_t cpu) {
-    int64_t cpu_mask = 1 << cpu;
+void affinitze_to_cpu(unsigned int cpu) {
+    int64_t cpu_mask = 1LLU << cpu;
     int result = sched_setaffinity(0, sizeof(long long), (const cpu_set_t *) &cpu_mask);
     if (result != 0) {
-        error(1, result, "Could not set affinity. Exiting...");
-        exit(1);
+        error(1, errno, "Could not pin to cpu %i. Exiting...", cpu);
     }
 }
 
 
-void print_args(int64_t duration, int64_t granularity, int64_t cpu) {
-    printf("cpu: %li\n", cpu);
-    printf("duration: %lus\n", duration/NANOS_IN_SEC);
-    printf("report interval: %lims\n", granularity/1000000);
+void print_args(struct program_args *args) {
+    int cpu = args->cpu;
+    printf("Target cpu: ");
+    if (cpu >= 0) printf("%i\n", args->cpu);
+    else puts("not pinned");
+    
+    printf("Duration: %lus\n", args->duration/NANOS_IN_SEC);
+    printf("Report interval: %lims\n", args->granularity/1000000);
 }
-
